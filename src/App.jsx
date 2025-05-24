de34a2e9e37231c108615ddaf6cde4e899b61eb3
Migue2210 <pazhucatoursoperaciones@gmail.com>
2025-05-24 18:17:12 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido al Registro de Ventas de Migue</h1>
+      <p>Este proyecto está desplegado correctamente en Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
